b164e29c2ed502e3d9d60b7b7ed2d2d9e2f9df9f
nereacal <44270078+nereacal@users.noreply.github.com>
2021-04-01 22:47:36 +0200

    Create Example_no_validation_input.c

---
+++ C input validation/Example_no_validation_input.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int a;
+    float b;
+    char c;
+
+
+    printf("Introduce un valor entero para la variable A : \n");
+    scanf("%i",&a);
+    printf("Introduce un valor flotante para la variable B : \n");
+    scanf("%f",&b);
+    printf("Introduce un caracter para la variable C : \n");
+    scanf("%c",&c);
+    printf("El valor de A es : %i\n",a);
+    printf("El valor de B es : %f\n",b);
+    printf("El valor de C es : %c\n",c);
+
+    return 0;
+}
